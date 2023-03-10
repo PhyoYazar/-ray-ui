f25a67bfe733696c451043623a4eccdf7c915e3d
Phyo Yarzar <phyoyazar592@gmail.com>
2023-03-10 14:41:11 +0630

    setup

---
+++ src/lib.ts
@@ -0,0 +1,2 @@
+export * from "./components/badge";
+export * from "./components/button";
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // optimizeDeps: {
+  //   include: ["react/jsx-runtime"],
+  // },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ["src"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      lib: path.resolve(__dirname, "./src/lib.ts"),
+    },
+  },
+  // for building
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src", "lib.ts"),
+      name: "@ray/ui",
+      formats: ["es", "umd"],
+      fileName: (format) => `ray.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
